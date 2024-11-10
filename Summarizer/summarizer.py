f2786f7ceac1a9f1779b8b5fe7e602b0f454bfab
AliCamlibel <aliocamlibel@gmail.com>
2024-11-09 21:25:16 -0500

    added sql to summarizer

--- Summarizer/summarizer.py
+++ Summarizer/summarizer.py
@@ -1,6 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import os
 import nltk
+import psycopg2
+import glob
 
 nltk.download('punkt')
 nltk.download('punkt_tab')
@@ -11,6 +13,8 @@ MODEL = 'philschmid/bart-large-cnn-samsum'
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModelForSeq2SeqLM.from_pretrained(MODEL)
+
+conn = psycopg2.connect(database='fercsummary', host='localhost', port='5432', user='alic', password='FercSummary!')
 
 
 def chunk_text(sentences):
@@ -55,23 +59,52 @@ def summarize(text):
 
     return ' '.join(chunk_summaries)
 
+def already_summarized(project_id, extracted_file_name):
+    cursor = conn.cursor()
+    cursor.execute('SELECT project_id FROM public.summary WHERE project_id = %s AND extracted_file_name = %s', (project_id, extracted_file_name))
+    exists = cursor.fetchone() is not None
+    cursor.close()
+    return exists
+
+
+def save_project(project_id, summarizations):
+    cursor = conn.cursor()
+
+    for summary in summarizations:
+        proj_folder = os.path.join('../APIScraper/downloads', project_id)
+        file_path = ''
+        for infile in glob.glob(os.path.join(proj_folder, f'{summary['file_name'].replace('.txt', '')}.*')):
+            file_path = infile
+            break
+
+        original_file_name = os.path.basename(file_path)
+        extracted_file_name = summary['file_name']
+
+        file_text = summary['file_text']
+        summary_text = summary['summary_text']
+
+        with open(file_path, 'rb') as f:
+            ba = f.read()
+            cursor.execute("INSERT INTO public.summary (summary_text, project_id, original_file_name, file_text, file_binary, extracted_file_name) VALUES (%s, %s, %s, %s, %s, %s)",
+                   (summary_text, project_id, original_file_name, file_text, ba, extracted_file_name))
+            
+    conn.commit()
+    cursor.close()
+
 
 summarizations = []
 
 project_ids = [name for name in os.listdir(FILES_FOLDER) if os.path.isdir(os.path.join(FILES_FOLDER, name))]
 
-
-summaries_dir = 'summaries'
-if not os.path.exists(summaries_dir):
-    os.mkdir(summaries_dir)
-
 for project_id in project_ids:
-    if os.path.exists(os.path.join('summaries', f'{project_id}.txt')):
-        print(f'Skipping {project_id}')
-        continue
-
     print(f'Processing {project_id}')
+
     for file_name in os.listdir(os.path.join(FILES_FOLDER, project_id)):
+
+        if already_summarized(project_id, file_name):
+            print(f'Skipping/Already Summarized : {file_name}')
+            continue
+
         print(f'Summarizing {file_name}')
         path = os.path.join(FILES_FOLDER, project_id, file_name)
 
@@ -83,13 +116,11 @@ for project_id in project_ids:
         except:
             continue
 
-        summarizations.append(summary)
+        summarizations.append({'summary_text': summary, 'file_name': file_name, 'file_text': text })
 
-    summary_path = os.path.join(summaries_dir, project_id + '.txt')
+    if len(summarizations) > 0:
+        save_project(project_id, summarizations)
 
-    with open(summary_path, 'w') as f:
-        f.write('\n\n'.join(summarizations))
+    summarizations = []
     
-
-#summary_of_summaries = summarize(' '.join(summarizations))
 
